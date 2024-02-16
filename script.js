635bc1e22067f8ff7ea3e075166d97d3abfbfb6f
aalemanyz67 <albertaleman67@gmail.com>
2024-02-16 10:32:58 -0600

    added javascript, html, and css to pizzawebsite

--- script.js
+++ script.js
@@ -10,5 +10,20 @@ let login = document.querySelector('.login-form');
 
 document.querySelector('#login').onclick = () =>{
     login.classList.toggle('active');
+    cart.classList.remove('active');
 
 }
+var swiper = new Swiper(".home-slider", {
+    autoplay:{
+        delay: 7500,
+        disableOnInteraction:false,
+    },
+    grabCursor: true,
+    loop: true,
+    centeredSlides:true,
+    navigation: {
+        nextE1: '.swiper-button-next',
+        prevE1: '.swiper-buttpm-prev',
+
+    },
+});
